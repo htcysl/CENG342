2600fb4e7a85b1c26c64bb5d7f7e47e41a6e999d
htcysl <maahhaticeuysal@gmail.com>
2023-03-16 21:09:41 +0300

    homework2
    git status
    git commit -mhomework2
    git commit -mhomework2

--- 20050111076.c
+++ 20050111076.c
@@ -1,9 +1,25 @@
 #include <hellomake.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+int main( ){
+    
+     
+     long  seed = 20050111076 ;
+     int lower = 1, upper = 100 ;
+     int range = upper-lower+1   ;   
+    
+     srand(seed) ;   
+        
+     // generated random number between ? 
+     for(int i=0;i<10 ;i++) {
+       printf("%5d\n",rand()%range)  ;    
+ }   
 
     //calling a function from header file
     myPrintHelloMake();
+     
+
 
     return 0 ;
 }
